c7285a7586eb9430590587c51248e7bf9fcdb6e2
Bridget Bailey <bbailey@pandora.com>
2019-12-18 22:33:45 -0800

    Updates

---
+++ precache-manifest.4fe01efd17508040bc3a2fb59420bbdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32ae33d0878d9fe05df8",
+    "url": "/GameFest2/static/js/runtime~main.32ae33d0.js"
+  },
+  {
+    "revision": "3ec6b0481a444816cb47",
+    "url": "/GameFest2/static/js/main.3ec6b048.chunk.js"
+  },
+  {
+    "revision": "23005d2f705db32e9da6",
+    "url": "/GameFest2/static/js/1.23005d2f.chunk.js"
+  },
+  {
+    "revision": "3ec6b0481a444816cb47",
+    "url": "/GameFest2/static/css/main.f27983b6.chunk.css"
+  },
+  {
+    "revision": "0e4f9696baa4095806d7464922619c5e",
+    "url": "/GameFest2/index.html"
+  }
+];
